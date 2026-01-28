558706137ff2c17de531db8a1928a63c808967f3
Charles Sarrazin-Boucher <cboucher@cegepgranby.qc.ca>
2026-01-28 13:52:57 -0500

    Retrait de l'exemple Newsletter en .Net 8.0

--- Newsletter.8.0/Newsletter.Web/ViewModels/NewsletterIndexVM.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GeniusChuck.Newsletter.Web.ViewModels
-{
-    public class NewsletterIndexVM
-    {
-        public IEnumerable<NewsletterSubscriberVM> Subscribers { get; set; } = new List<NewsletterSubscriberVM>();
-
-        public NewsletterRegisterVM Register { get; set; } = new NewsletterRegisterVM();
-    }
-}
