faf71084b9b19cd685e363294456c549303d246b
Charles Sarrazin-Boucher <cboucher@cegepgranby.qc.ca>
2026-02-11 17:45:50 -0500

    Ajout des classes de services Departement et Pays

---
+++ Exercices-Formulaire/Exercices-Formulaire-Web/Services/DepartementsService.cs
@@ -0,0 +1,42 @@
+﻿using Exercices_Formulaire_Web.Data;
+using Exercices_Formulaire_Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exercices_Formulaire_Web.Services
+{
+    public class DepartementsService(ApplicationDbContext context)
+    {
+        public async Task<List<Departement>> GetAllAsync()
+        {
+            return await context.Departements.ToListAsync();
+        }
+
+        public async Task<int> AddAsync(Departement departement)
+        {
+            context.Departements.Add(departement);
+            return await context.SaveChangesAsync();
+        }
+
+        public async Task<int> UpdateAsync(Departement departement)
+        {
+            context.Departements.Update(departement);
+            return await context.SaveChangesAsync();
+        }
+
+        public async Task<Departement?> FindAsync(int id)
+        {
+            return await context.Departements.FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            return await context.Departements.AnyAsync(e => e.Id == id);
+        }
+
+        public async Task<int> RemoveAsync(Departement departement)
+        {
+            context.Departements.Remove(departement);
+            return await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Exercices-Formulaire/Exercices-Formulaire-Web/Services/PaysService.cs
@@ -0,0 +1,15 @@
+﻿using Exercices_Formulaire_Web.Data;
+using Exercices_Formulaire_Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exercice_Formulaire_Web.Services
+{
+    public class PaysService(ApplicationDbContext context)
+    {
+
+        public async Task<List<Pays>> GetAll()
+        {
+            return await context.Pays.ToListAsync();
+        }
+    }
+}
